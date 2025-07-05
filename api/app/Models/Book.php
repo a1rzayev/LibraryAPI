dfe41a59729dc864c656fb1c8c4c2e150cdd293a
a1rzayev <asifrzayev2006@gmail.com>
2025-07-06 03:11:11 +0400

    Convert id to guid

--- api/app/Models/Book.php
+++ api/app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
@@ -13,4 +14,34 @@ class Book extends Model
         'title',
         'author'
     ];
+
+    /**
+     * Boot function from Laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the auto-incrementing key type.
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
 }
