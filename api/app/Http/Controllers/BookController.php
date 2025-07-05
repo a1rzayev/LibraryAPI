e42290c8264f0446baf05a5ea0474c9ea7617b2b
a1rzayev <asifrzayev2006@gmail.com>
2025-07-06 02:33:14 +0400

    Create & connect BookController

--- api/app/Http/Controllers/BookController.php
+++ api/app/Http/Controllers/BookController.php
@@ -3,23 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+    // GET api/books
     public function index()
     {
-        //
+        return Book::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
+    // POST api/books
     public function store(Request $request)
     {
-        //
+        $validated_book = $request->validate([
+            'title' => 'required|string|nax:256',
+            'author' => 'required|string|nax:256',
+        ]);
+        return Book::create($validated_book);
     }
 
     /**
--- api/routes/web.php
+++ api/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::apiResource('books', BookController::class);
