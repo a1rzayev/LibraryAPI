8403500d3faeb3854c94d90df9ff73c73bb306fa
a1rzayev <asifrzayev2006@gmail.com>
2025-07-06 11:48:34 +0400

    Create User model and table in db

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'address',
+        'role',
+        'is_active',
+        'membership_expires_at',
     ];
 
     /**
@@ -43,6 +48,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_active' => 'boolean',
+            'membership_expires_at' => 'date',
         ];
     }
 }
